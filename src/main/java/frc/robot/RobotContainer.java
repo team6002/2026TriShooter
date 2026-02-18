42e6fd7750cf75d9556aad584a49c9062ac03100
DragonCoder283 <roblox.player283@gmail.com>
2026-02-18 18:52:21 -0500

    merge

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.autos.trench.*;
+// import frc.robot.autos.Auto;
 import frc.robot.autos.unused.hump.AUTO_120;
 import frc.robot.commands.ShootFuelSim;
 // import frc.robot.autos.hump.*;
